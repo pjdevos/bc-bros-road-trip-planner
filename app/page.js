ab8821b8caf854ab05fd63c5ba9732ec1bb7c719
pjdevos <pjdevos@gmail.com>
2025-07-10 14:41:50 +0200

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,9 @@
+import BCRoadTripPlanner from '../components/BCRoadTripPlanner'
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gray-50">
+      <BCRoadTripPlanner />
+    </main>
+  )
+}
