972d8416da1799f715e2a22286e3f9a7295079e1
pjdevos <pjdevos@gmail.com>
2025-07-10 14:20:29 +0200

    Delete app/app/import './globals.css'  export const metadata = {   title: 'BC Bros Road Trip Planner',   description: '10 legends, 10 days, 1 epic camper van adventure through British Columbia', }  export default function RootLayout({ children }) {   return (     <html lang="en">       <body>{children}</body>     </app directory

--- app/app/import './globals.css'  export const metadata = {   title: 'BC Bros Road Trip Planner',   description: '10 legends, 10 days, 1 epic camper van adventure through British Columbia', }  export default function RootLayout({ children }) {   return (     <html lang="en">       <body>{children}</body>     </app/page.js
+++
@@ -1,9 +0,0 @@
-import BCRoadTripPlanner from '../components/BCRoadTripPlanner'
-
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-gray-50">
-      <BCRoadTripPlanner />
-    </main>
-  )
-}
